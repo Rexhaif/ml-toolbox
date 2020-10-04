4f7cb781c3445dd0cf5dc442de70d43678756504
Daniil Larionov <rexhaif.io@gmail.com>
2020-10-04 17:54:16 +0000

    Added password changing script

--- docker/chpwd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export HASHED_PASSWORD=$(python hashpwd.py $1)
-print($HASHED_PASSWORD)
-sed -i "s/# c.NotebookApp.password = u''/c.NotebookApp.password = u'$HASHED_PASSWORD'/g" /jupyter/jupyter_notebook_config.py
--- docker/hashpwd.py
+++ docker/hashpwd.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
-import sys
+import os
+import argparse as ap
+
 from notebook.auth import passwd
 
-print(passwd(sys.argv[1]))
+
+def replace_password_line(hashed_password: str, config_file: str):
+    with open(config_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    for i in range(len(lines)):
+        if "c.NotebookApp.password " in lines[i]:
+            lines[i] = f"c.NotebookApp.password = '{hashed_password}'"
+
+    with open(config_file, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line + os.linesep)
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser(prog='hashpwd.py', description="Helper script for changing jupyter notebook password")
+    parser.add_argument("-p", "--path", type=str, required=False, default=f"{os.environ['HOME']}/.jupyter/jupyter_notebook_config.py")
+    parser.add_argument("--password", type=str, required=True)
+
+    args: ap.Namespace = parser.parse_args()
+    hashed_password = passwd(args.password)
+    print("Your hashed password is: ", hashed_password)
+    print("Writing it to the config file: ", args.path)
+    replace_password_line(hashed_password, args.path)
+
+
